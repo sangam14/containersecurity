6dd00b776392e7b71211f698dffb8c817cef51b2
Sangam Biradar <sangam14@users.noreply.github.com>
2023-03-08 12:38:09 +0530

    add-vistor-counter

--- components/footer.js
+++ components/footer.js
@@ -34,6 +34,8 @@ export default function Footer({ twitter, linkedin, github }) {
             <p>Code Samples and Excercises Licensed Under Apache 2.0</p>
            
           </div>
+
+          <a href="https://visitorbadge.io/status?path=https%3A%2F%2Fkubedaily.com%2Fcontainersecurity%2F"><img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fkubedaily.com%2Fcontainersecurity%2F&labelColor=%23f47373&countColor=%23697689&style=flat" /></a>
         </li>
       </ul>
       <GitHubButton href="https://github.com/sangam14/containersecurity" data-color-scheme="no-preference: light; light: light_high_contrast; dark: dark_dimmed;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star sangam14/containersecurity on GitHub">Star</GitHubButton>
